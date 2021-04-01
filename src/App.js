360ae1cbd9702bb31405ea6f616fdd83941169db
Jaden <jaden.wei2003@gmail.com>
2021-03-31 19:51:55 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "./App.css";
+
+const socket = io.connect("http://localhost:5000");
 
 function App() {
+  const [msg, setMsg] = useState("");
+  const [chat, setChat] = useState([]);
+  const [name, setName] = useState();
+
+  const reqName = () => {
+    const n = prompt("What is your name?");
+    if (!n) {
+      reqName();
+    }
+    setName(n);
+  };
+
+  useEffect(() => {
+    reqName();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    socket.on("chat message", ({ name, msg }) => {
+      setChat([...chat, { name, msg }]);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chat]);
+
+  const onMessageSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("chat message", { name, msg });
+    setMsg("");
+  };
+
+  const renderChat = () => {
+    return chat.map(({ name, msg }, index) => (
+      <div key={index}>
+        <p>
+          {name}: {msg}
+        </p>
+      </div>
+    ));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chat-box">{chat.length > 0 ? renderChat() : ""}</div>
+
+      <div className="text-box">
+        <form onSubmit={onMessageSubmit}>
+          <input
+            name="msg"
+            onChange={(e) => {
+              setMsg(e.target.value);
+            }}
+            value={msg}
+          />
+          <button>Send</button>
+        </form>
+      </div>
     </div>
   );
 }
