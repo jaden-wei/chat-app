95808ea7d45022421b7a6a825b175a8f9431e56b
Jaden <jaden.wei2003@gmail.com>
2021-04-15 20:34:54 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: ["http://192.168.86.177:3000", "http://localhost:3000"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  const { id } = socket.client;
+
+  socket.on("chat message", ({ name, msg }) => {
+    io.emit("chat message", { id, name, msg });
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
