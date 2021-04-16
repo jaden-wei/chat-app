93618d58dd6359f27498b901d53786d9ca2fc097
Jaden <jaden.wei2003@gmail.com>
2021-04-15 21:23:55 -0700

    remove ip

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: ["http://192.168.86.177:3000", "http://localhost:3000"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
